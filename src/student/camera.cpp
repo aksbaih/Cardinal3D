3e37edbe3c6c709b73675e89c65300ffc4d9be9c
Akram Sbaih <akram@stanford.edu>
2023-02-26 15:02:42 -0800

    implemented and tested

--- src/student/camera.cpp
+++ src/student/camera.cpp
@@ -18,5 +18,14 @@ Ray Camera::generate_ray(Vec2 screen_coord) const {
     // Tip: compute the ray direction in view space and use
     // the camera space to world space transform (iview) to transform the ray back into world space.
 
-    return Ray();
+    const float sensorHeight = 2.f * tanf(Radians(vert_fov) * 0.5f);
+    const float sensorWidth = sensorHeight * aspect_ratio;
+    const Vec2 sensorDims(sensorWidth, sensorHeight);
+    const Vec2 sensorPoint(screen_coord * sensorDims - 0.5f * sensorDims);
+    const Vec3 sensorPointCameraSpace(sensorPoint.x, sensorPoint.y, -1.f);
+    /* Ray starts from pinhole (origin) in the direction of the sensor point. */
+    Ray ray(Vec3(0.f, 0.f, 0.f), sensorPointCameraSpace);
+    /* But we want it in the world space instead. */
+    ray.transform(iview);
+    return ray;
 }
