65d00ed756aff83cd5229839cf1216f92ca6d4e3
Akram Sbaih <akram@stanford.edu>
2023-02-28 12:58:04 -0800

    triangles work

--- src/student/tri_mesh.cpp
+++ src/student/tri_mesh.cpp
@@ -20,30 +20,47 @@ Trace Triangle::hit(const Ray& ray) const {
 
     // Vertices of triangle - has postion and surface normal
     // See rays/tri_mesh.h for a description of this struct
-    
+
     Tri_Mesh_Vert v_0 = vertex_list[v0];
     Tri_Mesh_Vert v_1 = vertex_list[v1];
     Tri_Mesh_Vert v_2 = vertex_list[v2];
 
-    // here just to avoid unused variable warnings, students should remove the following three lines.
+    // here just to avoid unused variable warnings, students should remove the following three
+    // lines.
     (void)v_0;
     (void)v_1;
     (void)v_2;
-    
+
     // TODO (PathTracer): Task 2
     // Intersect this ray with a triangle defined by the above three points.
     // Intersection should yield a ray t-value, and a hit point (u,v) on the surface of the triangle
 
     // You'll need to fill in a "Trace" struct describing information about the hit (or lack of hit)
 
-    Trace ret;
-    ret.origin = ray.point;
-    ret.hit = false;       // was there an intersection?
-    ret.distance = 0.0f;   // at what distance did the intersection occur?
-    ret.position = Vec3{}; // where was the intersection?
-    ret.normal = Vec3{};   // what was the surface normal at the intersection?
-                           // (this should be interpolated between the three vertex normals)
-    return ret;
+    /* Refer to https://stanford-cs248.github.io/Cardinal3D/pathtracer/ray_triangle_intersection for
+     * documentation on the following formulae. */
+    const Vec3 e1(v_1.position - v_0.position);
+    const Vec3 e2(v_2.position - v_0.position);
+    const Vec3 s(ray.point - v_0.position);
+    const Vec3 sXe2(cross(s, e2));
+    const Vec3 e1Xd(cross(e1, ray.dir));
+    const float denomFactor = 1.f / dot(e1Xd, e2);
+    const float u = -1.f * dot(sXe2, ray.dir) * denomFactor;
+    const float v = dot(e1Xd, s) * denomFactor;
+    const float t = -1.f * dot(sXe2, e1) * denomFactor;
+    /* Barycentric coords of hit point. */
+    const float bc1 = u;
+    const float bc2 = v;
+    const float bc0 = 1.f - bc1 - bc2;
+
+    /* Cases of no hit. */
+    if(isinf(denomFactor) || bc0 < 0.f || bc1 < 0.f || bc2 < 0.f || t < ray.dist_bounds.x ||
+       t > ray.dist_bounds.y)
+        return Trace{false};
+
+    /* Otherwise, populate the hit appropriately. */
+    return Trace{true, t, ray.point + ray.dir * t,
+                 bc0 * v_0.normal + bc1 * v_1.normal + bc2 * v_2.normal, ray.point};
 }
 
 Triangle::Triangle(Tri_Mesh_Vert* verts, unsigned int v0, unsigned int v1, unsigned int v2)
